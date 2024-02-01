67d99f4f3ea4b3a9b6f859bf82bc063da96b6a41
et120 <e.trotter120@gmail.com>
2024-02-01 15:27:20 -0800

    hosting test changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}",
-    "./*.{html,js}",
+    "./scripts/**/*.js",
+    "./index.html",
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
